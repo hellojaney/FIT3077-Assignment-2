b6f4b8df1045b824b67000051034a054a5b6b9ca
mattgueit <mattgueit@gmail.com>
2017-04-29 18:39:01 +1000

    add weatherframecollection

---
+++ weatherframecollection.py
@@ -0,0 +1,14 @@
+from weatherframe import WeatherFrame
+
+class WeatherFrameCollection:
+    def __init__(self):
+        self.wFrameList = []
+
+    def addFrame(self, newFrame):
+        self.wFrameList.append(newFrame)
+
+    def clearAllFrames(self):
+        for wFrame in self.wFrameList:
+            wFrame.closeFrame(wFrame.frame)
+        self.wFrameList = []
+
